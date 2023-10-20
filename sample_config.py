2cfe58cee07b8e17d1ab6564a8af02fee94a79ac
ZoidsRobot <113953968+ZoidsRobot@users.noreply.github.com>
2023-10-20 16:39:44 +0700

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -6,7 +6,7 @@ load_dotenv("config.env" if os.path.isfile("config.env") else "sample_config.env
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN', '5620597185:AAFP-9SMEH8W0dl-sApENxh20hG3yg2UVAQ')
 API_ID = int(os.environ.get('API_ID', '9774346'))
-SESSION_STRING = os.environ.get('SESSION_STRING', '')
+SESSION_STRING = os.environ.get('SESSION_STRING', 'BQCVJQoAJYK4FMp3bZHiTLd2a2c85MbiuNepruQnKskbQJ0b3Eisch6iyxtFkyTp7gvIpM90WnowX4OOt1UDZjCTtwrNgagWutvRWiyqf-APKKGbKSx15WirqMGd3UY22jBi14MsTBwou3PWQCf8cQ6BIy4aOwRNEqd8usO5HJjlqEfLtyw4KaXnlUrPz1Ct-1jV19kg3d6C_H7jTQabMBDAhBoTTf6qw_s92eickc7xYnPyiePOCWMch0iqeTnD0ZvXtknaZMhurzqqUnnE4R0mxWZWJRAjwGFjAjCzysGK7NfFtYpQ9nCLoZpAbfOxgCXzoLN0AcmtVDd0RZtei-r3JVHODQAAAAAzai5fAA')
 API_HASH = os.environ.get('API_HASH', 'a92aed7d74654a563af4b07efbcd88e9')
 USERBOT_PREFIX = os.environ.get('USERBOT_PREFIX', '.')
 PHONE_NUMBER = os.environ.get('PHONE_NUMBER')
